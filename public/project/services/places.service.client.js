ec2cd3a161d66191126391dbcdde5d0125f1b798
Vivek <shankaracharya.v@husky.neu.edu>
2016-03-04 04:22:56 -0800

    Project - POC

--- public/project/services/places.service.client.js
+++ public/project/services/places.service.client.js
@@ -4,16 +4,42 @@
         .factory("PlacesService", placesService);
 
     function placesService($http) {
-        var apiKey = "AIzaSyBTSIqcdFHPEW1HnivRTd6ZM76dhClMnp4";
+        //var apiKey = "AIzaSyBTSIqcdFHPEW1HnivRTd6ZM76dhClMnp4";
 
         var api = {
             getPlace: getPlace
         };
         return api;
 
+        function randomString(length, chars) {
+            var result = '';
+            for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+            return result;
+        }
+
         function getPlace(name, location, callback) {
-            $http.get("https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + name + "+restaurants+in+" + location + "&key=" + apiKey )
-                .success(callback);
+
+            var method = 'GET';
+            var url = 'http://api.yelp.com/v2/search';
+            var params = {
+                callback: 'angular.callbacks._0',
+                location: location,
+                oauth_consumer_key: 'jizYpyrilakp14_FxkEttA', //Consumer Key
+                oauth_token: 'TVyqnWMKB4XVvcm8pExEqOtdvrcc7jHp', //Token
+                oauth_signature_method: "HMAC-SHA1",
+                oauth_timestamp: new Date().getTime(),
+                oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+                term: name + "+restaurant",
+                limit: 3
+            };
+            var consumerSecret = '2CMDMAwV7iHtDWW87c9DEJhuon0'; //Consumer Secret
+            var tokenSecret = 'FrG920RNO4gV6MWrJShu0icF31M'; //Token Secret
+
+            var signature = oauthSignature.generate(method, url, params, consumerSecret, tokenSecret, { encodeSignature: false});
+            params['oauth_signature'] = signature;
+            //console.log(params);
+            $http.jsonp(url, {params: params})
+                .then(callback);
         }
     }
 })();
--- public/project/views/search/search.controller.js
+++ public/project/views/search/search.controller.js
@@ -4,12 +4,18 @@
         .controller("SearchController", SearchController);
 
     function SearchController($scope, $rootScope, PlacesService){
+        //var container = document.getElementById('search');
         $scope.searchResults = null;
+
         $scope.searchPlaces = function(formDetails) {
             PlacesService.getPlace(formDetails.placeName, formDetails.placeLocation, function(response) {
-                $scope.searchResults = response;
+                console.log(response.data.businesses);
+                $scope.searchResults = angular.copy(response.data.businesses);
             });
-            console.log($scope.searchResults);
+        };
+
+        $scope.clear = function() {
+            $scope.searchResults = null;
         };
     }
 })();
