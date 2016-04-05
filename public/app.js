ec46a8713780b10d80d9eb0dc3f604ce0ce14d0f
Vivek <shankaracharya.v@husky.neu.edu>
2016-04-05 13:37:34 -0700

    minor tweak

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
-"use strict";
-module.exports = function(app) {
-    var UserModel = require("./assignments/server/models/user.model.js")(app);
-
-    require("./assignments/server/services/user.server.service.js")(app, UserModel);
-};
+//"use strict";
+//module.exports = function(app) {
+//    var UserModel = require("./assignments/server/models/user.model.server.js")(app);
+//
+//    require("./assignments/server/services/user.server.service.js")(app, UserModel);
+//};
--- public/assignments/server/app.js
+++ public/assignments/server/app.js
@@ -1,8 +1,8 @@
 "use strict";
 module.exports = function(app, db, mongoose) {
-    var userModel = require("./models/user.model.js")(db, mongoose);
-    var formModel = require("./models/form.model.js")(db, mongoose);
-    var fieldModel = require("./models/field.model.js")(db, mongoose);
+    var userModel = require("./models/user.model.server.js")(db, mongoose);
+    var formModel = require("./models/form.model.server.js")(db, mongoose);
+    var fieldModel = require("./models/field.model.server.js")(db, mongoose);
 
     var userService = require("./services/user.service.server.js") (app, userModel);
     var formService = require("./services/form.service.server.js") (app, formModel);
